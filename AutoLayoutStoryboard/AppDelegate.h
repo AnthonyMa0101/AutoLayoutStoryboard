f748f5c6baf8b6322d7abf36b1fb7022804ada5d
AnthonyMa0101 <[anthony.ma111@gmail.com]>
2016-07-05 15:09:34 -0400

    working product

---
+++ AutoLayoutStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutStoryboard
+//
+//  Created by Anthony Ma on 2016-07-05.
+//  Copyright © 2016 Anthony Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutStoryboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutStoryboard
+//
+//  Created by Anthony Ma on 2016-07-05.
+//  Copyright © 2016 Anthony Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
